14a1d645971f86689c543dab727abf8805df6e6b
guwangtu <3338400895@qq.com>
2024-03-30 16:01:23 +0800

    1

---
+++ scripts/argument.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+def parser():
+    parser = argparse.ArgumentParser(description="attack")
+
+    parser.add_argument(
+        "--todo", choices=["train", "test"], default="train", help="train|test"
+    )
+    parser.add_argument("--device", default="0", type=str, help="0123")
+
+    parser.add_argument("--dataset", default="face_dataset")
+    parser.add_argument("--dataset2", default=None)
+    parser.add_argument("--epoches", type=int, default=10)
+    parser.add_argument("--save_each_epoch", type=int, default=5)
+    parser.add_argument("--save_path", default="checkpoint/face1")
+    parser.add_argument("--lr", default=5e-5)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--load_path", default=None)
+    parser.add_argument("--load_epoch", type=int, default=0)
+
+    parser.add_argument("--adv_train", type=bool, default=False)
+    parser.add_argument("--atk_eps", default=8 / 255)
+    parser.add_argument("--atk_alpha", default=2 / 225)
+    parser.add_argument("--atk_steps", type=int, default=10)
+
+    return parser.parse_args()
--- scripts/train.py
+++ scripts/train.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-
+import os
 import sys
 
 import torchattacks
@@ -15,136 +15,203 @@ import numpy as np
 
 from tqdm import tqdm
 
+from argument import parser
 
-device = "cuda:0"
 
+class Trainer:
+    def __init__(self, args, atk):
+        self.args = args
+        self.atk = atk
+        self.device = "cuda:" + args.device
 
-def train(model, train_loader, optimizer, criterion, adv_train = False, atk = None):
-    model.train()
-    total_loss = 0
-    train_corrects = 0
-    train_sum = 0
-    for i, (image, label) in enumerate(tqdm(train_loader)):
-        image = image.to(device)
-        label = label.to(device)
-        optimizer.zero_grad()
+    def train(self, model, train_loader, val_loader, adv_train=False):
+        args = self.args
+        atk = self.atk
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
 
-        if adv_train:
-            adv_image = atk(image, label)
-            target = model(adv_image)
+        save_path = args.save_path
+        int_files = [int(file) for file in os.listdir(save_path)]
+        if len(int_files) == 0:
+            save_path = os.path.join(save_path, "1")
         else:
-            target = model(image)
-        loss = criterion(target, label)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-        max_value, max_index = torch.max(target, 1)
-        pred_label = max_index.cpu().numpy()
-        true_label = label.cpu().numpy()
-        train_corrects += np.sum(pred_label == true_label)
-        train_sum += pred_label.shape[0]
-    return total_loss / float(len(train_loader)), train_corrects / train_sum
+            save_path = os.path.join(save_path, str(max(int_files) + 1))
 
+        for epoch in range(args.load_epoch, args.epoches):
+            train_loss, train_acc = self.train_step(
+                model, train_loader, optimizer, criterion
+            )
+            print(
+                "epoch"
+                + str(epoch + 1)
+                + "  train_loss:"
+                + str(train_loss)
+                + "  train_acc:"
+                + str(train_acc)
+            )
+            if adv_train:
+                train_loss, train_acc = self.train_step(
+                    model, train_loader, optimizer, criterion, adv_train=True
+                )
+                print(
+                    "epoch"
+                    + str(epoch + 1)
+                    + "  adv_train_loss:"
+                    + str(train_loss)
+                    + "  adv_train_acc:"
+                    + str(train_acc)
+                )
 
-def evaluate(model, test_loader, criterion,adv_test=False,atk = None):
-    model.eval()
-    corrects = eval_loss = 0
-    test_sum = 0
-    for image, label in tqdm(test_loader):
-        image = image.to(device)
-        label = label.to(device)
-        if adv_test:
-            image = atk(image, label)
-        with torch.no_grad():
-            pred = model(image)
-            loss = criterion(pred, label)
-            eval_loss += loss.item()
-            max_value, max_index = torch.max(pred, 1)
+            if (epoch + 1) % args.save_each_epoch == 0:
+                self.evaluate(model, val_loader, criterion, adv_test=True, atk=atk)
+                torch.save(
+                    model.state_dict(), save_path + "/epoch" + str(epoch + 1) + ".pt"
+                )
+        self.evaluate(model, val_loader, criterion, adv_test=True, atk=atk)
+        torch.save(
+            model.state_dict(), save_path + "/final_epoch" + str(args.epoches) + ".pt"
+        )
+
+    def train_step(self, model, train_loader, optimizer, criterion, adv_train=False):
+        args = self.args
+        atk = self.atk
+        device = self.device
+
+        model.train()
+
+        total_loss = 0
+        train_corrects = 0
+        train_sum = 0
+
+        for i, (image, label) in enumerate(tqdm(train_loader)):
+            image = image.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+
+            if adv_train:
+                adv_image = atk(image, label)
+                target = model(adv_image)
+            else:
+                target = model(image)
+            loss = criterion(target, label)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            max_value, max_index = torch.max(target, 1)
             pred_label = max_index.cpu().numpy()
             true_label = label.cpu().numpy()
-            corrects += np.sum(pred_label == true_label)
-            test_sum += np.sum(pred_label == true_label) + np.sum(
-                pred_label != true_label
+            train_corrects += np.sum(pred_label == true_label)
+            train_sum += pred_label.shape[0]
+            break
+
+        return total_loss / float(len(train_loader)), train_corrects / train_sum
+
+    def evaluate(self, model, val_loader, criterion, adv_test=False, atk=None):
+        test_loss, d, test_acc = self.evaluate_step(model, val_loader, criterion)
+        print("val_loss:" + str(test_loss) + "  val_acc:" + str(test_acc))
+        if adv_test:
+            test_loss, d, test_acc = self.evaluate_step(
+                model, val_loader, criterion, adv_test=True, atk=atk
             )
-    return eval_loss / float(len(test_loader)), corrects, corrects / test_sum
+            print("adv_val_loss:" + str(test_loss) + "  adv_val_acc:" + str(test_acc))
+
+    def evaluate_step(self, model, val_loader, criterion, adv_test=False, atk=None):
+        device = self.device
+
+        model.eval()
+        corrects = eval_loss = 0
+        test_sum = 0
+        for image, label in tqdm(val_loader):
+            image = image.to(device)
+            label = label.to(device)
+            if adv_test:
+                image = atk(image, label)
+            with torch.no_grad():
+                pred = model(image)
+                loss = criterion(pred, label)
+                eval_loss += loss.item()
+                max_value, max_index = torch.max(pred, 1)
+                pred_label = max_index.cpu().numpy()
+                true_label = label.cpu().numpy()
+                corrects += np.sum(pred_label == true_label)
+                test_sum += np.sum(pred_label == true_label) + np.sum(
+                    pred_label != true_label
+                )
+        return eval_loss / float(len(val_loader)), corrects, corrects / test_sum
 
 
-if __name__ == "__main__":
-    print("start training")
-
-    batch_size = 64
-    learning_rate = 5e-5
-    epoches = 10
-
-    # 数据增强
-
-    train_transform = transforms.Compose(
-        [
-            transforms.RandomRotation(20),  # 随机旋转角度
-            transforms.ColorJitter(brightness=0.1),  # 颜色亮度
-            transforms.Resize([224, 224]),  # 设置成224×224大小的张量
-            transforms.ToTensor(),  # 将图⽚数据变为tensor格式
-            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-            # std=[0.229, 0.224, 0.225]),
-        ]
-    )
-
-    val_transform = transforms.Compose(
-        [
-            transforms.Resize([224, 224]),
-            transforms.ToTensor(),  # 将图⽚数据变为tensor格式
-        ]
-    )
-
-    dataset_path = "face_dataset"
-    train_path = dataset_path + "/train"
-    val_path = dataset_path + "/test"
-    train_data = datasets.ImageFolder(train_path, transform=train_transform)
-    val_data = datasets.ImageFolder(val_path, transform=val_transform)
-    train_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    val_loader = data.DataLoader(val_data, batch_size=batch_size, shuffle=True)
+def main(args):
 
     model = models.resnet50(pretrained=True)
     model.fc = torch.nn.Linear(2048, 2)
-    #m_state_dict = torch.load("d4/epoch16.pt")
-    #model.load_state_dict(m_state_dict)
+
+    batch_size = args.batch_size
+    device = "cuda:" + str(args.device)
+
+    if args.load_path:
+        load_path = args.load_path
+        m_state_dict = torch.load(load_path)
+        model.load_state_dict(m_state_dict)
+
     model = model.to(device)
 
-    atk = PGD(model, eps=8 / 255, alpha=2 / 225, steps=10, random_start=True)
+    atk = PGD(
+        model,
+        eps=args.atk_eps,
+        alpha=args.atk_alpha,
+        steps=args.atk_steps,
+        random_start=True,
+    )
     atk.set_normalization_used(mean=[0, 0, 0], std=[1, 1, 1])
- 
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    for epoch in range(epoches):
-        train_loss, train_acc = train(model, train_loader, optimizer, criterion, adv_train=True,atk = atk)
-        print(
-            "epoch"
-            + str(epoch + 1)
-            + "  train_loss:"
-            + str(train_loss)
-            + "  train_acc:"
-            + str(train_acc)
+    trainer = Trainer(args, atk)
+
+    if args.todo == "train":
+
+        dataset_path = args.dataset
+        train_path = dataset_path + "/train"
+        val_path = dataset_path + "/test"
+        save_path = args.save_path
+        if not os.path.isdir(save_path):
+            os.mkdir(save_path)
+
+        train_transform = transforms.Compose(
+            [
+                transforms.RandomRotation(20),  # 随机旋转角度
+                transforms.ColorJitter(brightness=0.1),  # 颜色亮度
+                transforms.Resize([224, 224]),  # 设置成224×224大小的张量
+                transforms.ToTensor(),
+                # transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                # std=[0.229, 0.224, 0.225]),
+            ]
         )
-        if epoch % 1 == 0:
-            test_loss, d, test_acc = evaluate(model, val_loader, criterion)
-            print(
-                "epoch"
-                + str(epoch + 1)
-                + "  val_loss:"
-                + str(test_loss)
-                + "  val_acc:"
-                + str(test_acc)
-            )
-            test_loss, d, test_acc = evaluate(model, val_loader, criterion, adv_test=True,atk = atk)
-            print(
-                "epoch"
-                + str(epoch + 1)
-                + "  adv_val_loss:"
-                + str(test_loss)
-                + "  adv_val_acc:"
-                + str(test_acc)
-            )
-            torch.save(model.state_dict(), "epoch" + str(epoch + 1) + ".pt")
-    torch.save(model.state_dict(), "epoch" + str(epoches) + ".pt")
-    print("finish")
+
+        val_transform = transforms.Compose(
+            [
+                transforms.Resize([224, 224]),
+                transforms.ToTensor(),
+            ]
+        )
+
+        train_data = datasets.ImageFolder(train_path, transform=train_transform)
+        train_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        val_data = datasets.ImageFolder(val_path, transform=val_transform)
+        val_loader = data.DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
+        trainer.train(model, train_loader, val_loader, args.adv_train)
+
+    elif args.todo == "test":
+
+        dataset_path = args.dataset
+        val_path = dataset_path + "/test"
+        val_data = datasets.ImageFolder(val_path, transform=val_transform)
+        val_loader = data.DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
+        trainer.evaluate()
+
+
+if __name__ == "__main__":
+    
+    args = parser()
+
+    main(args)
