0f05743251671486ce330545b01b0066533a0615
guwangtu <3338400895@qq.com>
2024-06-19 21:13:25 +0800

    '0'

--- scripts/create_degrad_img.py
+++ scripts/create_degrad_img.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageFilter
 import os
 from tqdm import tqdm
-
+import torchvision.transforms as transforms
 
 # 读取图片集
 src_dir = '/data/user/shx/Generate_image_detection/LASTED/data/face_dataset/test'
-output_dir = '/data/user/shx/Generate_image_detection/face_test_blur'
+output_dir = '/data/user/shx/Generate_image_detection/face_test_70_com'
 
 quality = 65
 blur = 5
@@ -21,11 +21,37 @@ os.makedirs(output_dir, exist_ok=True)
 
 
 # Blur
-for subfolder in os.listdir(src_dir):
+'''for subfolder in os.listdir(src_dir):
     subfolder_dir=os.path.join(src_dir,subfolder)
+    os.mkdir(output_dir+'/'+subfolder, exist_ok=True)
     for file in os.listdir(subfolder_dir):
         with Image.open(os.path.join(subfolder_dir, file)) as img:
             name, ext = os.path.splitext(file)
             save_path = os.path.join(output_dir+'/'+subfolder, name + '.jpg')
             img = img.filter(ImageFilter.GaussianBlur(blur))
-            img.save(save_path)
+            img.save(save_path)'''
+for subfolder in os.listdir(src_dir):
+    subfolder_dir=os.path.join(src_dir,subfolder)
+    os.mkdir(output_dir+'/'+subfolder, exist_ok=True)
+    for file in os.listdir(subfolder_dir):
+        with Image.open(os.path.join(subfolder_dir, file)) as img:
+            name, ext = os.path.splitext(file)
+            save_path = os.path.join(output_dir+'/'+subfolder, name + '.jpg')
+
+            image_pil = transforms.ToPILImage()(img) 
+            image_pil.save(output_dir+'/'+subfolder, name + '.jpg', optimize=True, quality=70)
+ 
+
+output_dir = '/data/user/shx/Generate_image_detection/face_test_35_com'
+os.makedirs(output_dir, exist_ok=True)
+for subfolder in os.listdir(src_dir):
+    subfolder_dir=os.path.join(src_dir,subfolder)
+    os.mkdir(output_dir+'/'+subfolder, exist_ok=True)
+    for file in os.listdir(subfolder_dir):
+        with Image.open(os.path.join(subfolder_dir, file)) as img:
+            name, ext = os.path.splitext(file)
+            save_path = os.path.join(output_dir+'/'+subfolder, name + '.jpg')
+
+            image_pil = transforms.ToPILImage()(img) 
+            image_pil.save(output_dir+'/'+subfolder, name + '.jpg', optimize=True, quality=35)
+ 
