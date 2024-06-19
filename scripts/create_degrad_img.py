a77d4bd6c641b768ffc26295ae6ef650faa71ae6
guwangtu <3338400895@qq.com>
2024-06-19 21:44:02 +0800

    '0'

--- scripts/create_degrad_img.py
+++ scripts/create_degrad_img.py
@@ -40,7 +40,7 @@ for subfolder in os.listdir(src_dir):
 
             image_pil = img
             image_pil.save(
-                output_dir + "/" + subfolder, name + ".jpg", optimize=True, quality=70
+                save_path+ name + ".jpg", optimize=True, quality=70
             )
 
 
@@ -56,5 +56,5 @@ for subfolder in os.listdir(src_dir):
 
             image_pil = img
             image_pil.save(
-                output_dir + "/" + subfolder, name + ".jpg", optimize=True, quality=35
+                save_path+name + ".jpg", optimize=True, quality=35
             )
